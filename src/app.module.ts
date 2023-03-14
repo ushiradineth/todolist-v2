5700a5308e2a1f4b12059ab8b2cf8275b1ce58de
Ushira Dineth <ushiradineth@gmail.com>
2023-03-14 17:00:43 +0530

    testing graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService, MyResolver } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
     TodosModule,
-    // GraphQLModule.forRoot({
-    //   autoSchemaFile: 'schema.gql',
-    // }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://ushira:ushira@cluster.9yj1wvz.mongodb.net/?retryWrites=true&w=majority',
     ),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MyResolver],
 })
 export class AppModule {}
