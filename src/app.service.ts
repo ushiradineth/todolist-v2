0ee81395906f0dd2a28fb2a7ad5421ad9ff071d3
Ushira Dineth <ushiradineth@gmail.com>
2023-03-15 13:10:06 +0530

    removed test coding

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Resolver, Query } from '@nestjs/graphql';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Welcome to NestJS todo demo app --- Get /todo to see all todos  --- Post /todo/create to create todo --- Delete /todo/delete/{id} to delete todo --- Update /todo/update/{id}/ to update todo ';
-  }
-}
-
-@Resolver()
-export class MyResolver {
-  @Query(() => String)
-  hello(): string {
-    return 'Hello World!';
   }
 }
