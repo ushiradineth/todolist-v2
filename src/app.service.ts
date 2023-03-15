d0863618d7d198c57877bdb3df8a93fbc7cd583c
Ushira Dineth <ushiradineth@gmail.com>
2023-03-15 15:21:13 +0530

    added documentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to NestJS todo demo app --- Get /todo to see all todos  --- Post /todo/create to create todo --- Delete /todo/delete/{id} to delete todo --- Update /todo/update/{id}/ to update todo ';
+  getHello(): object {
+    return {
+      Title: "Welcome to NestJS todo demo app",
+      Todos: {
+        path: "/todos",
+        GET: {
+          use: "Get all todos",
+        },
+        POST: {
+          use: "Create a todo",
+          input: {
+            name: "string",
+            todo: "string",
+          },
+        },
+        PUT: {
+          use: "Update a todo",
+          input: {
+            id: "string",
+            todo: "string",
+          },
+        },
+        DELETE: {
+          use: "Delete a todo",
+          input: {
+            id: "string",
+          },
+        },
+      },
+    };
   }
 }
