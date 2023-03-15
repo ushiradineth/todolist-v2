2f8d83693eed8be562171b477e34ee03c8e735c2
Ushira Dineth <ushiradineth@gmail.com>
2023-03-15 13:25:54 +0530

    fixed delete todo

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { CreateTodoDto } from './dto/create-todo.dto';
-import { DeleteTodoDto } from './dto/delete-todo.dto';
-import { UpdateTodoDto } from './dto/update-todo.dto';
-import { Todo, TodoDocument } from './todos.model';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { CreateTodoDto } from "./dto/create-todo.dto";
+import { DeleteTodoDto } from "./dto/delete-todo.dto";
+import { UpdateTodoDto } from "./dto/update-todo.dto";
+import { Todo, TodoDocument } from "./todos.model";
 
 @Injectable()
 export class TodosService {
@@ -14,7 +14,7 @@ export class TodosService {
     return this.todoModel.find().exec();
   }
 
-  async createTodo(createTodoDto: CreateTodoDto): Promise<Todo> {
+  createTodo(createTodoDto: CreateTodoDto): Promise<Todo> {
     const createdTodo = new this.todoModel(createTodoDto);
     return createdTodo.save();
   }
@@ -29,9 +29,10 @@ export class TodosService {
   }
 
   async deleteTodo(deleteTodoDto: DeleteTodoDto): Promise<Todo> {
-    const deletedTodo = await this.todoModel.findOneAndUpdate({
+    const deletedTodo = await this.todoModel.findByIdAndDelete({
       _id: deleteTodoDto.id,
     });
-    return deletedTodo.delete();
+
+    return deletedTodo;
   }
 }
