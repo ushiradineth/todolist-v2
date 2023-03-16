99b260b045dfdbc523eb623dc97464a05cc18f4f
Ushira Dineth <ushiradineth@gmail.com>
2023-03-16 11:58:55 +0530

    added swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder } from "@nestjs/swagger";
+import { SwaggerModule } from "@nestjs/swagger/dist";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const config = new DocumentBuilder().setTitle("TODO API").setDescription("TODO API Documentation").setVersion("1.0").build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
 bootstrap();
