15c5120ee022496f000e5df347710cb7c44f261e
Ushira Dineth <ushiradineth@gmail.com>
2023-03-18 20:57:25 +0530

    added cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const config = new DocumentBuilder().setTitle("TODO API").setDescription("TODO API Documentation").setVersion("1.0").build();
   const document = SwaggerModule.createDocument(app, config);
