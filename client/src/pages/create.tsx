0be7a6ef3fc5de0cd24cafb9058a07a2489f7eb6
Ushira Dineth <ushiradineth@gmail.com>
2023-03-19 23:08:11 +0530

    added create todo page mutation

--- client/src/pages/create.tsx
+++ client/src/pages/create.tsx
@@ -3,9 +3,13 @@ import Input from "@/components/Input";
 import Title from "@/components/Title";
 import Head from "next/head";
 import React from "react";
-import { gql, useMutation } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import { Container } from "@/components/styles/Container.styled";
 import { Card } from "@/components/styles/Card.styled";
+import { CREATE_TODO } from "@/util/graphql/mutation";
+import Spinner from "@/components/Spinner";
+import Error from "@/components/Error";
+import BackButton from "@/components/BackButton";
 
 export default function Create() {
   const [name, setName] = React.useState("");
@@ -13,9 +17,8 @@ export default function Create() {
 
   const [createTodo, { data, loading, error }] = useMutation(CREATE_TODO);
 
-  React.useEffect(() => {
-    console.log(data, loading, error);
-  }, [data, loading, error]);
+  if (loading) return <Spinner />;
+  if (error) return <Error error={error.message} />;
 
   return (
     <>
@@ -27,6 +30,7 @@ export default function Create() {
       </Head>
       <Container>
         <Card>
+          <BackButton />  
           <Title text="Create a todo!" />
           <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
             <Input id="Name" maxlength={50} placeholder="Name" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} />
