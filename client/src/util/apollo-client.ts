53eeb38f3e24bd75284b9c4c485d5bce77e95454
Ushira Dineth <ushiradineth@gmail.com>
2023-03-19 23:15:39 +0530

    added env

--- client/src/util/apollo-client.ts
+++ client/src/util/apollo-client.ts
@@ -5,10 +5,12 @@ let client: ApolloClient<any> | null = null;
 export const getClient = () => {
   // create a new client if there's no existing one
   // or if we are running on the server.
+
   if (!client || typeof window === "undefined") {
     client = new ApolloClient({
       link: new HttpLink({
-        uri: process.env.API_URL,
+        uri: process.env.NEXT_PUBLIC_API_URL,
+        // NEXT_PUBLIC_API_URL=http://localhost:3000/graphql
       }),
       cache: new InMemoryCache(),
     });
