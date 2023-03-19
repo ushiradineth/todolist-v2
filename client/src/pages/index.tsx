76623246673b94c4098b15e22d23276d43fffaab
Ushira Dineth <ushiradineth@gmail.com>
2023-03-19 23:09:00 +0530

    added home page with all todos

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,8 @@
-"use client";
-
-import ClientOnly from "@/components/ClientOnly";
-import Todos from "@/components/Todos";
+import AllTodos from "@/components/AllTodos";
+import { Card } from "@/components/styles/Card.styled";
+import { Container } from "@/components/styles/Container.styled";
+import { StyledLink } from "@/components/styles/Link.styled";
+import Title from "@/components/Title";
 import Head from "next/head";
 
 export default function Home() {
@@ -13,11 +14,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <ClientOnly>
-          <Todos />
-        </ClientOnly>
-      </main>
+      <Container>
+        <Card>
+          <Title text="All todos!" />
+          <AllTodos />
+          <Actions />
+        </Card>
+      </Container>
     </>
+  );
+}
+
+function Actions() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <StyledLink href={"/create"}>Create a Todo</StyledLink>
+      <StyledLink href={"/update"}>Update a Todo</StyledLink>
+      <StyledLink href={"/delete"}>Delete a Todo</StyledLink>
+      <StyledLink href={"/search"}>Search for a Todo</StyledLink>
+    </div>
   );
 }
