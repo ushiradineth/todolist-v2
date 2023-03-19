09f5c7121ae04ecd01fa689919e4f7288cd3e495
Ushira Dineth <ushiradineth@gmail.com>
2023-03-19 23:08:44 +0530

    added ssr testing page

--- client/src/pages/ssr.tsx
+++ client/src/pages/ssr.tsx
@@ -1,19 +1,24 @@
 import Head from "next/head";
-import { getAllTodos } from "@/util/graphql/query";
+import { GET_ALL_TODOS } from "@/util/graphql/query";
 import { getClient } from "@/util/apollo-client";
+import { Card } from "@/components/styles/Card.styled";
+import { Container } from "@/components/styles/Container.styled";
+import Title from "@/components/Title";
+import { TodoItem } from "@/components/AllTodos";
+import BackButton from "@/components/BackButton";
 
 export const getServerSideProps = async () => {
   const client = getClient();
-  const { data } = await client.query({ query: getAllTodos });
+  const { data } = await client.query({ query: GET_ALL_TODOS });
 
   return {
     props: {
-      data,
+      todos: data.Todos as Todo[],
     },
   };
 };
 
-export default function Home({ data }) {
+export default function Home(props: { todos: Todo[] }) {
   return (
     <>
       <Head>
@@ -22,7 +27,17 @@ export default function Home({ data }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>{JSON.stringify(data)}</main>
+      <Container>
+        <Card>
+          <BackButton />
+          <Title text="All todos!" />
+          <div>
+            {props.todos.map((item, index) => (
+              <TodoItem key={index} todo={item} />
+            ))}
+          </div>
+        </Card>
+      </Container>
     </>
   );
 }
