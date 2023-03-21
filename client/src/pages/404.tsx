f88764443ff0de74acd026e8bc695ccc4ba14732
Ushira Dineth <ushiradineth@gmail.com>
2023-03-21 12:42:25 +0530

    added custom error pages

---
+++ client/src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@/components/Layout";
+import Title from "@/components/Title";
+import React from "react";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <Title text="No page found"></Title>
+    </Layout>
+  );
+}
---
+++ client/src/pages/500.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@/components/Layout";
+import Title from "@/components/Title";
+import React from "react";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <Title text="Server-side error occurred"></Title>
+    </Layout>
+  );
+}
