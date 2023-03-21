b3ccb3ef130d97e9c5e3dfacaf20631827920005
Ushira Dineth <ushiradineth@gmail.com>
2023-03-21 10:51:23 +0530

    fixed margin error

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { getClient } from "@/util/apollo-client";
+import GlobalStyled from "@/util/StyledComponents/Global.styled";
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = getClient();
 
   return (
     <ApolloProvider client={client}>
+      <GlobalStyled />
       <Component {...pageProps} />
     </ApolloProvider>
   );
