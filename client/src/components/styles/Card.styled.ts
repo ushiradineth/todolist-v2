f2c8c06938596c653cac54b04717e82560729415
Ushira Dineth <ushiradineth@gmail.com>
2023-03-21 11:28:18 +0530

    minor ui change

--- client/src/components/styles/Card.styled.ts
+++ client/src/components/styles/Card.styled.ts
@@ -4,8 +4,6 @@ export const Card = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 24px;
-  border: 4px;
   border-radius: 12px;
   background-color: white;
   padding: 20px;
