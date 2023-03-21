cedca5c5d706d3341bcc14bde179e9303eb1c356
Ushira Dineth <ushiradineth@gmail.com>
2023-03-21 12:02:39 +0530

    removed viewport meta tag

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -12,7 +12,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content="NestJS API Demo" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
