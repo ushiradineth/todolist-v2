eb462186d32e25cd938c083e2d66bb1b054f386f
Ushira Dineth <ushiradineth@gmail.com>
2023-03-21 12:02:26 +0530

    updated meta tags and styling

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,17 +1,17 @@
 import Head from "next/head";
 import styled from "styled-components";
+import HomeButton from "./HomeButton";
+import { Layout } from "./Layout";
 
 const StyledContainer = styled.div`
   display: flex;
   height: 100vh;
+  width: 100vw;
   font-family: monospace;
   align-items: center;
   justify-content: center;
-  border-radius: 8px;
-  padding-left: 16px;
-  padding-right: 16px;
   background-color: #18181b;
-  font-size: 36px;
+  font-size: 24px;
   color: white;
 `;
 
@@ -20,11 +20,13 @@ const Error = (props: { error: string }) => {
     <>
       <Head>
         <title>Error</title>
-        <meta name="description" content="NestJS API Demo" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <StyledContainer>{props.error}</StyledContainer>
+      <>
+        <HomeButton />
+        <StyledContainer>
+          <p style={{ padding: "20px" }}>{props.error}</p>
+        </StyledContainer>
+      </>
     </>
   );
 };
