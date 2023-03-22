caf407c9bbba4f40bbd8428ec85325112b028ca3
Ushira Dineth <ushiradineth@gmail.com>
2023-03-22 17:03:27 +0530

    rearranged the ui

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import AllTodos from "@/components/AllTodos";
 import Button from "@/components/Button";
 import { Layout } from "@/components/Layout";
+import { Card } from "@/components/styles/Card.styled";
+import { Container } from "@/components/styles/Container.styled";
 import { StyledLink } from "@/components/styles/Link.styled";
 import Title from "@/components/Title";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import styled from "styled-components";
 
 export default function Home() {
   return (
@@ -13,30 +16,51 @@ export default function Home() {
       <Head>
         <title>NestJS API Demo Frontend with NextJS</title>
       </Head>
-      <Layout>
-        <Title text="All todos!" />
-        <AllTodos />
+      <Container>
         <Actions />
-      </Layout>
+        <Card>
+          <Title text="All todos!" />
+          <AllTodos />
+        </Card>
+      </Container>
     </>
   );
 }
 
 function Actions() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <p>{status}</p>
-      <p>{session?.user.email}</p>
-      <StyledLink href={"/create"}>Create a Todo</StyledLink>
-      <StyledLink href={"/update"}>Update a Todo</StyledLink>
-      <StyledLink href={"/delete"}>Delete a Todo</StyledLink>
-      <StyledLink href={"/search"}>Search for a Todo</StyledLink>
-      <StyledLink href={"/ssr"}>SSR</StyledLink>
-      <Button onClick={() => router.push("/auth")} text={"sign in"} disabled={false} />
-      <Button onClick={() => signOut()} text={"sign out"} disabled={false} />      
-    </div>
+    <Sidebar>
+      <Card>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <StyledLink href={"/create"}>Create a Todo</StyledLink>
+          <StyledLink href={"/update"}>Update a Todo</StyledLink>
+          <StyledLink href={"/delete"}>Delete a Todo</StyledLink>
+          <StyledLink href={"/search"}>Search for a Todo</StyledLink>
+          <StyledLink href={"/ssr"}>SSR</StyledLink>
+        </div>
+      </Card>
+      <Profile />
+    </Sidebar>
   );
 }
+
+function Profile() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  if (status === "unauthenticated" && router.pathname !== "/404" && router.pathname !== "/500") router.push("/auth");
+
+  return (
+    <Card>
+      <p>{session?.user.email}</p>
+      <Button onClick={() => signOut()} text={"Sign Out"} disabled={false} />
+    </Card>
+  );
+}
+
+const Sidebar = styled.div`
+  display: "flex";
+  flex-direction: "column";
+  justify-content: "start";
+  background-color: chartreuse;
+  height: 100vh;
+`;
