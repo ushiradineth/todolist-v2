e3333cb56bbb0b29e967a833e416f53883f006eb
Ushira Dineth <ushiradineth@gmail.com>
2023-03-22 16:06:58 +0530

    added rerouting for unauthed users

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
 import HomeButton from "./HomeButton";
@@ -6,7 +7,10 @@ import { Container } from "./styles/Container.styled";
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const router = useRouter();
-  
+  const { status } = useSession();
+
+  if (status === "unauthenticated" && router.pathname !== "/404" && router.pathname !== "/500") router.push("/auth");
+
   return (
     <Container>
       <Card>
