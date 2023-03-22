0509108bbf399215b5bbefd21c7ba4a5d35958b1
Ushira Dineth <ushiradineth@gmail.com>
2023-03-22 10:18:24 +0530

    removed user controller

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { UserController } from "./user.controller";
 import { User, UserSchema } from "./user.model";
 import { UserResolver } from "./user.resolver";
 import { UserService } from "./user.service";
@@ -9,6 +8,5 @@ import { UserService } from "./user.service";
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   exports: [UserModule],
   providers: [UserService, UserResolver],
-  controllers: [UserController],
 })
 export class UserModule {}
