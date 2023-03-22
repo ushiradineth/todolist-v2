ef857b59bb641cfec64b18c7528c808fd53cb91f
Ushira Dineth <ushiradineth@gmail.com>
2023-03-22 15:55:31 +0530

    testing global themes

--- client/src/util/StyledComponents/Theme.styled.tsx
+++ client/src/util/StyledComponents/Theme.styled.tsx
@@ -1,26 +1,25 @@
 export const base = {
   ​​breakpoints: ['768px'],
-  space: ['0px', '2px', '4px', '8px', '16px', '32px', '64px', ...],
+  space: ['0px', '2px', '4px', '8px', '16px', '32px', '64px'],
   fonts: {
     heading: 'Inter, system-ui, sans-serif',
     body: 'Inter, system-ui, sans-serif',
   },
-  fontSizes: ['12px', '14px', '16px', '20px', '24px', ...],
-  ...
+  fontSizes: ['12px', '14px', '16px', '20px', '24px'],
 }
+
 export const light = {
   primary: '#4851f4',
   background: '#ffffff',
   nav: '#f8f8f8',
   border: '#deebf1',
   text: '#202224',
-  ...
 }
+
 export const dark = {
   primary: '#4851f4',
   background: '#1f2023',
   nav: '#27282b',
   border: '#303236',
   text: '#f8f8f8',
-  ...
 }
