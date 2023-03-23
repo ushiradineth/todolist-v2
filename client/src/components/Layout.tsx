051b9762ad7786d475cba572e4b9e445eba43243
Ushira Dineth <ushiradineth@gmail.com>
2023-03-23 20:33:30 +0530

    added loading state for layout

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
 import HomeButton from "./HomeButton";
+import Spinner from "./Spinner";
 import { Card } from "./styles/Card.styled";
 import { Container } from "./styles/Container.styled";
 
@@ -9,6 +10,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
   const router = useRouter();
   const { status } = useSession();
 
+  if (status === "loading") return <Spinner />;
   if (status === "unauthenticated" && router.pathname !== "/404" && router.pathname !== "/500") router.push("/auth");
 
   return (
