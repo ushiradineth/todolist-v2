1a2c0295200f80e8a3be62a699a62ab7a4e787a1
Ushira Dineth <ushiradineth@gmail.com>
2023-03-23 20:36:25 +0530

    removed unused code

--- server/src/todos/todos.module.ts
+++ server/src/todos/todos.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Todo, TodoSchema } from './todos.model';
@@ -10,7 +9,6 @@ import { TodosResolver } from './todos.resolver';
     MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
   ],
   exports: [TodosModule],
-  controllers: [TodosController],
   providers: [TodosService, TodosResolver],
 })
 export class TodosModule {}
