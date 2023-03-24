e42d665e81089840e96a1825c3c700ca3bdd4711
Ushira Dineth <ushiradineth@gmail.com>
2023-03-24 11:49:32 +0530

    removed jwt override

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -1,37 +1,22 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import KeycloakProvider from "next-auth/providers/keycloak";
 import { getClient } from "@/util/apollo-client";
 import { AUTHENTICATE_USER } from "@/util/graphql/user/query";
-
-type User = {
-  [key: string]: any;
-  name: string;
-};
 
 export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
   callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token.user = user;
-      }
-      return Promise.resolve(token as User);
-    },
     session({ session, token }) {
-      if (token.user) {
-        const t = token.user as {
-          id: string;
-        };
+      session.user = {
+        id: token.sub,
+        email: token.email,
+        name: token.name,
+      };
 
-        session.user = {
-          id: t.id,
-          email: token.email,
-          name: token.name,
-        };
-      }
-      return Promise.resolve(session);
+      return session;
     },
   },
   providers: [
@@ -41,14 +26,13 @@ export const authOptions: NextAuthOptions = {
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
+      async authorize(credentials) {
         const client = getClient();
-        
         const { data } = await client.query({ query: AUTHENTICATE_USER, variables: { email: credentials?.email, password: credentials?.password } });
 
         if (!data) {
           return null;
-        }        
+        }
 
         return {
           id: data.UserAuthentication._id,
