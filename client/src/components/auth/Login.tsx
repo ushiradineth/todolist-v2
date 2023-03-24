7de28992101fdb14dc379e018857bbb4a9a266b9
Ushira Dineth <ushiradineth@gmail.com>
2023-03-24 17:55:51 +0530

    added react hook forms with yup validation for auth

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,62 +1,57 @@
 import toast from "@/util/Toast";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Button from "../Button";
 import Input from "../Input";
 import * as yup from "yup";
 import { PasswordLog, StyledForm } from "@/pages/auth";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { emailValidator, passwordValidator } from "@/util/validators";
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailValidation, setEmailValidation] = useState(false);
-  const [passwordValidation, setPasswordValidation] = useState(false);
+  const { register, watch } = useForm<InputType>({ resolver: yupResolver(schema) });
+  const formData = watch();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const router = useRouter();
 
-  useEffect(() => {
-    yup
-      .string()
-      .email()
-      .validate(email)
-      .then(() => !emailValidation && setEmailValidation(true))
-      .catch(() => emailValidation && setEmailValidation(false));
-
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [email]);
-
-  useEffect(() => {
-    yup
-      .string()
-      .required("No password provided.")
-      .min(8, "Password is too short - should be 8 characters minimum.")
-      .max(20, "Password is too long - should be 20 characters maximum.")
-      .matches(/[a-zA-Z]/, "Password can only contain Latin letters.")
-      .matches(/(?=.*[A-Z])/, "Password must have atleast one Uppercase Letter")
-      .matches(/(?=.*[0-9])/, "Password must have atleast one Number")
-      .matches(/(?=.*[!@#\$%\^&\*])/, "Password must have atleast one Special Character")
-      .validate(password)
-      .then(() => !passwordValidation && setPasswordValidation(true))
-      .catch(() => passwordValidation && setPasswordValidation(false));
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [password]);
-
-  const onLogin = async () => {
-    setLoading(true)
-    const login = await signIn("credentials", { email, password, callbackUrl: "/", redirect: false });
-    login?.status === 401 ? toast("Invalid Credentials", "warning") : login?.error && toast(login?.error, "error");
-    setLoading(false)
-    login?.ok && router.push("/");
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    schema
+      .validate(formData)
+      .then(async (data) => {
+        setError("");
+        setLoading(true);
+        const login = await signIn("credentials", { email: data.email, password: data.password, callbackUrl: "/", redirect: false });
+        login?.status === 401 ? toast("Invalid Credentials", "warning") : login?.error && toast(login?.error, "error");
+        setLoading(false);
+        login?.ok && router.push("/");
+      })
+      .catch((err) => error !== err && setError(err.message.toUpperCase()));
   };
 
   return (
-    <StyledForm>
-      <Input id="email" placeholder="Email" type="Email" onChange={(e) => setEmail(e.currentTarget.value)} />
-      <Input id="password" placeholder="Password" type="Password" minlength={8} maxlength={20} onChange={(e) => setPassword(e.currentTarget.value)} />
-      <PasswordLog password={password} />
-      <Button loading={loading} disabled={!emailValidation || !passwordValidation} text={"Login"} onClick={() => onLogin()} />
+    <StyledForm onSubmit={(e) => submitHandler(e)}>
+      <Input id="email" type="email" placeholder="Email" register={register("email")} />
+      <Input id="password" type="password" placeholder="Password" register={register("password")} />
+      {/* <PasswordLog password={formData.password || "error"} /> */}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <Button loading={loading} text={"Login"} />
     </StyledForm>
   );
 };
+
+type InputType = {
+  email: string;
+  password: string;
+};
+
+const schema = yup
+  .object()
+  .shape({
+    password: passwordValidator,
+    email: emailValidator,
+  })
+  .required();
--- client/src/components/auth/Registration.tsx
+++ client/src/components/auth/Registration.tsx
@@ -1,68 +1,56 @@
-import { StyledForm, PasswordLog } from "@/pages/auth";
+import { StyledForm } from "@/pages/auth";
 import { CREATE_USER } from "@/util/graphql/user/mutation";
 import toast from "@/util/Toast";
 import { useMutation } from "@apollo/client";
-import { useState, useEffect } from "react";
 import Button from "../Button";
 import Input from "../Input";
 import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { emailValidator, nameValidator, passwordValidator } from "@/util/validators";
+import { FormEvent, useState } from "react";
 
 export const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailValidation, setEmailValidation] = useState(false);
-  const [passwordValidation, setPasswordValidation] = useState(false);
-  const [nameValidation, setNameValidation] = useState(false);
   const [createUser, { loading }] = useMutation(CREATE_USER, { onError: (e) => toast(e.networkError ? "Error creating User." : "User already exists.", "error"), onCompleted: () => toast("User created!", "success") });
 
-  useEffect(() => {
-    yup
-      .string()
-      .email()
-      .validate(email)
-      .then(() => !emailValidation && setEmailValidation(true))
-      .catch(() => emailValidation && setEmailValidation(false));
+  const { register, watch } = useForm<InputType>({ resolver: yupResolver(schema) });
+  const formData = watch();
+  const [error, setError] = useState("");
 
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [email]);
-
-  useEffect(() => {
-    yup
-      .string()
-      .required("Name password provided.")
-      .min(1, "Name is too short - should be atleast 1 character minimum.")
-      .max(100, "Name is too long - should be 100 characters maximum.")
-      .validate(name)
-      .then(() => !nameValidation && setNameValidation(true))
-      .catch(() => nameValidation && setNameValidation(false));
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [name]);
-
-  useEffect(() => {
-    yup
-      .string()
-      .required("No password provided.")
-      .min(8, "Password is too short - should be 8 chars minimum.")
-      .matches(/[a-zA-Z]/, "Password can only contain Latin letters.")
-      .matches(/(?=.*[A-Z])/, "Password must have atleast one Uppercase Letter")
-      .matches(/(?=.*[0-9])/, "Password must have atleast one Number")
-      .matches(/(?=.*[!@#\$%\^&\*])/, "Password must have atleast one Special Character")
-      .validate(password)
-      .then(() => !passwordValidation && setPasswordValidation(true))
-      .catch(() => passwordValidation && setPasswordValidation(false));
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [password]);
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    schema
+      .validate(formData)
+      .then(async (data) => {
+        setError("");
+        createUser({ variables: data });
+      })
+      .catch((err) => error !== err && setError(err.message.toUpperCase()));
+  };
 
   return (
-    <StyledForm>
-      <Input type="text" id="name" placeholder="Name" minlength={1} maxlength={100} onChange={(e) => setName(e.currentTarget.value)} />
-      <Input type="email" id="email" placeholder="Email" onChange={(e) => setEmail(e.currentTarget.value)} />
-      <Input type="password" id="password" placeholder="Password" minlength={8} maxlength={20} onChange={(e) => setPassword(e.currentTarget.value)} />
-      <PasswordLog password={password} />
-      <Button loading={loading} disabled={!emailValidation || !nameValidation || !passwordValidation} text={"Register"} onClick={() => createUser({ variables: { email, name, password } })} />
+    <StyledForm onSubmit={(e) => submitHandler(e)}>
+      <Input id="name" type="text" placeholder="Name" register={register("name")} />
+      <Input id="email" type="email" placeholder="Email" register={register("email")} />
+      <Input id="password" type="password" placeholder="Password" register={register("password")} />
+      {/* <PasswordLog password={formData.password || ""} /> */}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <Button loading={loading} text={"Register"} />
     </StyledForm>
   );
 };
+
+type InputType = {
+  email: string;
+  name: string;
+  password: string;
+};
+
+const schema = yup
+  .object()
+  .shape({
+    password: passwordValidator,
+    email: emailValidator,
+    name: nameValidator,
+  })
+  .required();
