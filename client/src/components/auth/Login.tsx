9a8efee88bee2aefa60d209bb72bcc9d41e98776
Ushira Dineth <ushiradineth@gmail.com>
2023-03-24 15:24:28 +0530

    removed test code

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -7,18 +7,6 @@ import Input from "../Input";
 import * as yup from "yup";
 import { PasswordLog, StyledForm } from "@/pages/auth";
 
-const loginSchema = yup.object({
-  email: yup.string().defined().email(),
-  password: yup
-    .string()
-    .required("No password provided.")
-    .min(8, "Password is too short - should be 8 chars minimum.")
-    .matches(/[a-zA-Z]/, "Password can only contain Latin letters.")
-    .matches(/(?=.*[A-Z])/, "Password must have atleast one Uppercase Letter")
-    .matches(/(?=.*[0-9])/, "Password must have atleast one Number")
-    .matches(/(?=.*[!@#\$%\^&\*])/, "Password must have atleast one Special Character"),
-});
-
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
